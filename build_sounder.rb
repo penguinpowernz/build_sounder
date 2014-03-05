2445e33db62923028a89724d7c39f6021849fd46
Robert McLeod <robert@autogrow.com>
2014-03-05 17:01:26 +1300

    refactored and added some a config file for the sound locations

--- build_sounder.rb
+++ build_sounder.rb
@@ -1,17 +1,33 @@
 require 'rspec/core/formatters/progress_formatter'
+require 'yaml'
 # dirty hack, see: https://github.com/carlhuda/bundler/issues/183
 $LOAD_PATH.concat Dir.glob("#{ENV['rvm_path']}/gems/#{ENV['RUBY_VERSION']}@global/gems/*/lib")
 
 class BuildSounder < RSpec::Core::Formatters::ProgressFormatter
 
-  SUCCESS_SOUND = "/usr/share/sounds/LinuxMint/stereo/desktop-login.ogg"
-  FAILURE_SOUND = "/usr/share/sounds/gnome/default/alerts/bark.ogg"
+  def initialize(output)
+    super(output)
+    @sounds = YAML.load_file("#{File.expand_path(File.dirname(__FILE__))}/sounds.yml")
+  end
 
   def dump_summary(duration, example_count, failure_count, pending_count)
+    super
     if failure_count.zero?
-      fork{ exec "mpg123", "-q" FAILURE_SOUND }
+      play @sounds[:success]
     else
-      fork{ exec "mpg123", "-q" SUCCESS_SOUND }
+      play @sounds[:failure]
+    end
+  end
+
+  def play(file)
+    puts "BuildSounder: Can't find #{file}" and
+      return if !File.exist?(file)
+    
+    case File.extname(file)
+    when ".mp3"
+      fork{ exec "mpg123", "-q", file }
+    when ".ogg"
+      fork{ exec "ogg123", "-q", file }
     end
   end
 
